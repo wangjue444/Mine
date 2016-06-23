33bcd87bb11158c068cc6733587c72bd7361445e
Jue Wang <wangjue444@126.com>
2016-06-23 11:14:09 +0800

    Add files via upload

---
+++ Snake/resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by NO71.RC
+//
+#define IDB_BITMAP1                     101
+#define IDB_BITMAP2                     102
+#define IDB_BITMAP3                     103
+#define IDB_BITMAP4                     104
+#define IDB_BITMAP5                     105
+#define IDB_BITMAP6                     106
+#define IDB_BITMAP7                     107
+#define IDB_BITMAP8                     108
+#define IDB_BITMAP9                     109
+#define IDR_MENU1                       110
+#define IDD_DIALOG1                     111
+#define IDD_DIALOG2                     112
+#define IDI_ICON1                       114
+#define IDB_BITMAP11                    122
+#define IDB_BITMAP10                    123
+#define IDB_BITMAP12                    124
+#define ID_40001                        40001
+#define ID_40002                        40002
+#define ID_40003                        40003
+#define ID_40004                        40004
+#define ID_40005                        40005
+#define ID_40006                        40006
+#define ID_40007                        40007
+#define ID_40008                        40008
+#define ID_40009                        40009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        125
+#define _APS_NEXT_COMMAND_VALUE         40010
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
